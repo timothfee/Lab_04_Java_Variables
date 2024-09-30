84f32fc30e33502c64ad2e7d0e577fa8f1b83322
Timothy Fee <keys333@proton.me>
2024-09-29 21:12:39 -0400

    Init Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
    public static void main(String[] args) {
-      int kids = 1;
-      boolean isRaining = true;
-      double gasGallonPrice = 3.25;
-      int favNumber = 17;
-      double shoeSize = 9.5;
-      int myBirthMonth = 6;
-      String myFullName = "Timothy Fee";
-      
+    
+    Scanner in = new Scanner(System.in);
+    double price = 0;
+    double shipping = 0;
 
-      System.out.println("My full name is: " + myFullName);
+    System.out.print("Enter the price: ");
+    price = in.nextDouble();
 
-  }
+    if (price >= 100) {
+      shipping = 0;
+    } 
+    else {
+      shipping = price * 0.02;
+    }
+    
+    System.out.println("Shipping cost: $" + shipping);
+    System.out.println("Total cost: $" + (price + shipping));
+
+   }
 }
